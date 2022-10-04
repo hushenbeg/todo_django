1c3e3aeb6bc3baa6e90afe72370b4a70e221f176
Hushen Beg <hushen.beg@gmail.com>
2022-10-04 06:05:14 +0530

    Update settings.py
    removed secrete key

--- todopro/settings.py
+++ todopro/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*(&j1ha5ty41=519&xkiekj5psmlem2ks9qy^@bd=om)q@mqi2'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
